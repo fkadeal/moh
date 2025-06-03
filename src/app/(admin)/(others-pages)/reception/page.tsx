37b3424fc0647c05d71f41349262c55d83fc693b
Fkadeal Mati... <44930374+fkadeal@users.noreply.github.com>
2025-06-03 10:21:16 +0300

    Update page.tsx
    ddddd

--- src/app/(admin)/(others-pages)/reception/page.tsx
+++ src/app/(admin)/(others-pages)/reception/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import ReceptionInterface from "@/components/ReceptionInterface";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
